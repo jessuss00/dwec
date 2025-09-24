434db395d93c72aa9860fb31ba802a3f847a2cc8
jessuss00 <jesusmadro23@gmail.com>
2025-09-24 13:00:04 +0200

    9

--- ud1_introduccion_js/Boletin2/script.js
+++ ud1_introduccion_js/Boletin2/script.js
@@ -1,8 +1,12 @@
-let colores = ["#F8BBD0", "#E1BEE7", "#D1C4E9", "#C5CAE9", "#BBDEFB", "#B3E5FC", "#B2EBF2", "#B2DFDB", "#C8E6C9", "#DCEDC8", "#F0F4C3", "#FFF9C4", "#FFECB3", "#FFE0B2", "#FFCCBC", "#D7CCC8", "#CFD8DC"];
-let ejercicios =  document.getElementsByClassName("ejercicio");
+let colores = ["#FFD1DC", "#FFE5B4", "#FFFACD", "#BFD8B8", "#AEC6CF", "#CBAACB", "#E3E4FA", "#AAF0D1", "#FF7F50", "#FFD8B1"
+];
+
+let ejercicios = document.getElementsByClassName("ejercicio");
+
 window.onload = function() {
-    for (let i = 0; i < ejercicios.length; i++) {
-        let colorAleatorio = Math.floor(Math.random() * colores.length);
-        ejercicios[i].style.backgroundColor = colores[colorAleatorio];
+    for(let i = 0; i < ejercicios.length; i++){
+        let colorAleatorio = Math.random() * colores.length;
+        console.log(colorAleatorio);
+        ejercicios[i].style.backgroundColor = colores[Math.floor(colorAleatorio)];
     }
 }
