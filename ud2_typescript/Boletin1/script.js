45b7b8d2cff3855dd45f0779a342792d6b57c3b9
jessuss00 <jesusmadro23@gmail.com>
2025-10-29 12:48:20 +0100

    9

--- ud2_typescript/Boletin1/script.js
+++ ud2_typescript/Boletin1/script.js
@@ -7,16 +7,53 @@ function ej1() {
 }
 //ejercicio 2
 function ej2() {
-    console.log("Validando formulario...");
-    var exReg = new RegExp("lo_que_sea");
-    if (document.getElementById("email") != null) {
-        var mensajOk = document.createElement("span");
-        var email = document.getElementById("email");
-        if (email && exReg.test(email.value)) {
-            mensajOk.textContent = "Email correcto";
-        }
-        else {
-            mensajOk.textContent = "Email incorrecto";
-        }
+    var regExp = new RegExp("[^\s@]+@[^\s@]+\.[^\s@]+$"); //Con fallos (hay que corregir)
+    //Aplico la validacion de la expresion regular
+    if (regExp.test($inputValue("email"))) {
+        //Si test devuelve true quiere decir que el texto "email" cumple la expresion reg.
+        $writeNode("ok", "El email es valido"); //Aquí escribo
+        $writeNode("error", ""); //Aquí escribo tambien (lo que pasa que borra)
+    }
+    else {
+        //Si es false quiere decir que el texto "email" no cumple la expresion regular.
+        $writeNode("ok", ""); //Aquí escribo (borrando el contenido)
+        $writeNode("error", "El email no cumple la expresion"); //Aqui tambien escribo
+    }
+}
+//Helpers
+function $inputValue(id) {
+    var input = document.getElementById(id); //Lectura
+    var result = "";
+    if (input) {
+        result = input.value; //Recojo el valor (aqui es donde leo)
+    }
+    return result;
+}
+function $writeNode(id, msg) {
+    var nodo = document.getElementById(id); //Escritura
+    if (nodo) {
+        nodo.textContent = msg;
+    }
+}
+function suma(a, b) {
+    return a + b;
+}
+var suma2 = function (a, b) { return a + b; };
+//Ejercicio 3
+function ej3() {
+    var ventanaNueva = window.open("https://www.google.com");
+    //ventanaNueva?.document.write("Hola");
+}
+//Ejercicio 4
+function ej4() {
+    var regExp = new RegExp("^https:\/\/");
+    var url = $inputValue("url");
+    if (regExp.test(url)) {
+        window.location.href = url;
+    }
+    else {
+        $writeNode("error2", "Por favor introduzca una URL valida");
+        //Ej5
+        setTimeout(function () { return $writeNode("error2", ""); }, 5000);
     }
 }
--- ud2_typescript/Boletin1/script.ts
+++ ud2_typescript/Boletin1/script.ts
@@ -6,16 +6,61 @@ function ej1(): void {
     console.log("Hora anterior: " + (hora.getHours() - 1) + ":" + hora.getMinutes() + " hola");
 }
 //ejercicio 2
-function ej2(): void {
-    console.log("Validando formulario...");
-    let exReg: RegExp = new RegExp("lo_que_sea");
-    if(document.getElementById("email") != null){
-        let mensajOk = document.createElement("span");
-        let email: HTMLInputElement = document.getElementById("email") as HTMLInputElement;
-        if(email && exReg.test(email.value)){
-            mensajOk.textContent = "Email correcto";
-        }else{
-            mensajOk.textContent = "Email incorrecto";
-        }
+function ej2(){    
+    const regExp = new RegExp("[^\s@]+@[^\s@]+\.[^\s@]+$"); //Con fallos (hay que corregir)
+    
+    //Aplico la validacion de la expresion regular
+    if(regExp.test($inputValue("email"))){
+        //Si test devuelve true quiere decir que el texto "email" cumple la expresion reg.
+        $writeNode("ok","El email es valido") //Aquí escribo
+        $writeNode("error",""); //Aquí escribo tambien (lo que pasa que borra)
+    }else{
+        //Si es false quiere decir que el texto "email" no cumple la expresion regular.
+        $writeNode("ok","") //Aquí escribo (borrando el contenido)
+        $writeNode("error", "El email no cumple la expresion") //Aqui tambien escribo
     }
+}
+
+//Helpers
+function $inputValue(id: string) : string{
+    const input = document.getElementById(id) as HTMLInputElement; //Lectura
+    let result = "";
+
+    if(input){
+        result = input.value; //Recojo el valor (aqui es donde leo)
+    }
+
+    return result;
+}
+function $writeNode (id: string, msg: string) : void {
+    const nodo = document.getElementById(id) as HTMLElement; //Escritura
+
+    if(nodo){
+        nodo.textContent = msg;
+    }
+
+}
+function suma (a: number, b: number) : number{
+   return a+b;
+}
+const suma2 = (a:number, b:number) => a+b;
+
+//Ejercicio 3
+function ej3(){
+     let ventanaNueva = window.open("https://www.google.com");
+     //ventanaNueva?.document.write("Hola");
+}
+//Ejercicio 4
+
+function ej4(){
+    const regExp = new RegExp ("^https:\/\/");
+    const url = $inputValue("url");
+    if(regExp.test(url)){
+        window.location.href = url;
+    }else{
+        $writeNode("error2", "Por favor introduzca una URL valida")
+        //Ej5
+        setTimeout(()=> $writeNode("error2",""),5000);
+    }
+    
 }
